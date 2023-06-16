732f15d2f3eb3eb3427de4a813b2ce631efacdd3
Peter Morka <pcmorka@gmail.com>
2023-06-16 04:45:05 +0000

    terraform code update.

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
-/* Here, this Terraform resource will create an Amazon Web Services (AWS) VPC that will have a public subnet with two Availability Zones (AZs) for the web server tier, and two private subnets with two Availability Zones (AZs) for the RDS server tier. The public subnets will have Internet access through an Internet Gateway, while the private subnets will have internet access through a NAT Gateway*/.
-
 #create an Amazon Web Services (AWS) Virtual Private Cloud (a custom VPC )
-resource "aws_vpc" "wk22Project-vpc" {
+resource "aws_vpc" "wk22project-vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
@@ -14,101 +12,101 @@ resource "aws_vpc" "wk22Project-vpc" {
 }
 
 #create internet gateway to attach to VPC
-resource "aws_internet_gateway" "wk22Project-igw" {
-  vpc_id = aws_vpc.wk22Project-vpc.id
+resource "aws_internet_gateway" "wk22project-igw" {
+  vpc_id = aws_vpc.wk22project-vpc.id
 
   tags = {
-    Name = "wk22Project-igw"
+    Name = "wk22project-igw"
   }
 }
 
 #Create the first public subnet (public subnet 1) for the WebServer-Tier in the us-east-1a Availability Zone (AZ)
-resource "aws_subnet" "wk22Project-public1" {
-  vpc_id                  = aws_vpc.wk22Project-vpc.id
+resource "aws_subnet" "wk22project-public1" {
+  vpc_id                  = aws_vpc.wk22project-vpc.id
   cidr_block              = "10.10.1.0/24"
   availability_zone       = var.az1a
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "wk22Project-public1"
+    Name = "wk22project-public1"
   }
 }
 
 #Create the second public subnet (public subnet 2) for the WebServer-Tier in the us-east-1b Availability Zone (AZ)
-resource "aws_subnet" "wk22Project-public2" {
-  vpc_id                  = aws_vpc.wk22Project-vpc.id
+resource "aws_subnet" "wk22project-public2" {
+  vpc_id                  = aws_vpc.wk22project-vpc.id
   cidr_block              = "10.10.2.0/24"
   availability_zone       = var.az1b
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "wk22Project-public2"
+    Name = "wk22project-public2"
   }
 }
 
 #create the first Elastic IP address (EIP) in an AWS VPC to assign to NAT Gateway 
-resource "aws_eip" "wk22Project-eip1" {
+resource "aws_eip" "wk22project-eip1" {
   vpc        = true
-  depends_on = [aws_vpc.wk22Project-vpc]
+  depends_on = [aws_vpc.wk22project-vpc]
   tags = {
-    Name = "wk22Project-eip1"
+    Name = "wk22project-eip1"
   }
 }
 
 #create the second Elastic IP address (EIP) in an AWS VPC to assign to NAT Gateway 
-resource "aws_eip" "wk22Project-eip2" {
+resource "aws_eip" "wk22project-eip2" {
   vpc        = true
-  depends_on = [aws_vpc.wk22Project-vpc]
+  depends_on = [aws_vpc.wk22project-vpc]
   tags = {
-    Name = "wk22Project-eip2"
+    Name = "wk22project-eip2"
   }
 }
 
 #create the first NAT Gateway in the VPC public subnet 1
-resource "aws_nat_gateway" "wk22Project-nat-gw1" {
-  depends_on    = [aws_eip.wk22Project-eip1]
-  allocation_id = aws_eip.wk22Project-eip1.id
-  subnet_id     = aws_subnet.wk22Project-public1.id
+resource "aws_nat_gateway" "wk22project-nat-gw1" {
+  depends_on    = [aws_eip.wk22project-eip1]
+  allocation_id = aws_eip.wk22project-eip1.id
+  subnet_id     = aws_subnet.wk22project-public1.id
   tags = {
-    Name = "wk22Project-nat-gw"
+    Name = "wk22project-nat-gw"
   }
 }
 
 #create the second NAT Gateway in the VPC public subnet 2
-resource "aws_nat_gateway" "wk22Project-nat-gw2" {
-  depends_on    = [aws_eip.wk22Project-eip2]
-  allocation_id = aws_eip.wk22Project-eip2.id
-  subnet_id     = aws_subnet.wk22Project-public2.id
+resource "aws_nat_gateway" "wk22project-nat-gw2" {
+  depends_on    = [aws_eip.wk22project-eip2]
+  allocation_id = aws_eip.wk22project-eip2.id
+  subnet_id     = aws_subnet.wk22project-public2.id
   tags = {
-    Name = "wk22Project-nat-gw"
+    Name = "wk22project-nat-gw"
   }
 }
 
 #Create the first private subnet (private subnet 1) for the RDS_Server-Tier in the us-east-1a Availability Zone (AZ)
-resource "aws_subnet" "wk22Project-private1" {
-  vpc_id            = aws_vpc.wk22Project-vpc.id
+resource "aws_subnet" "wk22project-private1" {
+  vpc_id            = aws_vpc.wk22project-vpc.id
   cidr_block        = "10.10.3.0/24"
   availability_zone = var.az1a
 
   tags = {
-    Name = "wk22Project-private1"
+    Name = "wk22project-private1"
   }
 }
 
 #Create the second private subnet (private subnet 2) for the RDS_Server-Tier in the us-east-1b Availability Zone (AZ)
-resource "aws_subnet" "wk22Project-private2" {
-  vpc_id            = aws_vpc.wk22Project-vpc.id
+resource "aws_subnet" "wk22project-private2" {
+  vpc_id            = aws_vpc.wk22project-vpc.id
   cidr_block        = "10.10.4.0/24"
   availability_zone = var.az1b
 
   tags = {
-    Name = "wk22Project-private2"
+    Name = "wk22project-private2"
   }
 }
 
 #create public route table with route for internet gateway
-resource "aws_route_table" "wk22Project-public-rt" {
-  vpc_id = aws_vpc.wk22Project-vpc.id
+resource "aws_route_table" "wk22project-public-rt" {
+  vpc_id = aws_vpc.wk22project-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -116,43 +114,43 @@ resource "aws_route_table" "wk22Project-public-rt" {
   }
 
   tags = {
-    Name = "wk22Project-igw"
+    Name = "wk22project-igw"
   }
 }
 
 #public route table with public subnet associations 
 resource "aws_route_table_association" "wk22publicsubnet-route1" {
-  route_table_id = aws_route_table.wk22Project-public-rt.id
-  subnet_id      = aws_subnet.wk22Project-public1.id
+  route_table_id = aws_route_table.wk22project-public-rt.id
+  subnet_id      = aws_subnet.wk22project-public1.id
 }
 
 resource "aws_route_table_association" "wk22publicsubnet-route2" {
-  route_table_id = aws_route_table.wk22Project-public-rt.id
-  subnet_id      = aws_subnet.wk22Project-public2.id
+  route_table_id = aws_route_table.wk22project-public-rt.id
+  subnet_id      = aws_subnet.wk22project-public2.id
 }
 
 #create private route table with route for NAT gateway in the AWS VPC
-resource "aws_route_table" "wk22Project-private-rt1" {
+resource "aws_route_table" "wk22project-private-rt1" {
   vpc_id = aws_vpc.wk22Project-vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.wk22Project-nat-gw1.id
+    nat_gateway_id = aws_nat_gateway.wk22project-nat-gw1.id
   }
 
   tags = {
-    Name = "wk22Project-private-rt1"
+    Name = "wk22project-private-rt1"
   }
 }
 
 #private route table with private subnet associations     
 resource "aws_route_table_association" "wk22privatesubnet-route1" {
-  route_table_id = aws_route_table.wk22Project-private-rt1.id
-  subnet_id      = aws_subnet.wk22Project-private1.id
+  route_table_id = aws_route_table.wk22project-private-rt1.id
+  subnet_id      = aws_subnet.wk22project-private1.id
 }
 
 #create private route table with route for NAT gateway in the AWS VPC
-resource "aws_route_table" "wk22Project-private-rt2" {
+resource "aws_route_table" "wk22project-private-rt2" {
   vpc_id = aws_vpc.wk22Project-vpc.id
 
   route {
@@ -161,7 +159,7 @@ resource "aws_route_table" "wk22Project-private-rt2" {
   }
 
   tags = {
-    Name = "wk22Project-private-rt2"
+    Name = "wk22project-private-rt2"
   }
 }
 
@@ -174,10 +172,10 @@ resource "aws_route_table_association" "wk22privatesubnet-route2" {
 #create security groups that allows inbound traffic from the internet
 
 #Web Server Security Group
-resource "aws_security_group" "wk22Project-webserver-sg" {
-  name   = "wk22Project-webserver-sg"
+resource "aws_security_group" "wk22project-webserver-sg" {
+  name   = "wk22project-webserver-sg"
   description = "allow traffic from Webserver Tier & SSH" 
-  vpc_id = aws_vpc.wk22Project-vpc.id
+  vpc_id = aws_vpc.wk22project-vpc.id
 
   ingress {
     from_port   = 22
@@ -208,15 +206,15 @@ resource "aws_security_group" "wk22Project-webserver-sg" {
   }
 
   tags = {
-    Name = "wk22Project-webserver-sg"
+    Name = "wk22project-webserver-sg"
   }
 }
 
 #Database Server (MYSQL Server) Security Group
-resource "aws_security_group" "wk22Project-db-sg" {
-  name   = "wk22Project-db-sg"
+resource "aws_security_group" "wk22project-db-sg" {
+  name   = "wk22project-db-sg"
   description = "allow traffic from Webserver Tier & SSH" 
-  vpc_id = aws_vpc.wk22Project-vpc.id
+  vpc_id = aws_vpc.wk22project-vpc.id
 
   ingress {
     from_port   = 3306
@@ -232,15 +230,15 @@ resource "aws_security_group" "wk22Project-db-sg" {
   }
 
   tags = {
-    Name = "wk22Project-db-sg"
+    Name = "wk22project-db-sg"
   }
 }
 
 #Application Load Balancer Security Group
-resource "aws_security_group" "wk22Project-alb-sg" {
-  name   = "wk22Project-alb-sg"
+resource "aws_security_group" "wk22project-alb-sg" {
+  name   = "wk22project-alb-sg"
   description = "alb security group"
-  vpc_id = aws_vpc.wk22Project-vpc.id
+  vpc_id = aws_vpc.wk22project-vpc.id
 
   ingress {
     from_port   = 80
@@ -264,19 +262,19 @@ resource "aws_security_group" "wk22Project-alb-sg" {
   }
 
   tags = {
-    Name = "wk22Project-alb-sg"
+    Name = "wk22project-alb-sg"
   }
 }
 
 
 # Create database subnet group for RDS instance
 
-resource "aws_db_subnet_group" "wk22Project-dbsubnet-grp" {
-  name       = "wk22Project-dbsubnet-grp"
-  subnet_ids = [aws_subnet.wk22Project-private1.id, aws_subnet.wk22Project-private2.id]
+resource "aws_db_subnet_group" "wk22project-dbsubnet-grp" {
+  name       = "wk22project-dbsubnet-grp"
+  subnet_ids = [aws_subnet.wk22project-private1.id, aws_subnet.wk22project-private2.id]
 
   tags = {
-    Name = "wk22Project db subnet group"
+    Name = "wk22project db subnet group"
   }
 }
 
@@ -292,24 +290,24 @@ resource "aws_db_instance" "wk22project-db-instance" {
   #Set Database login credentials for the RDS Instance
   username               = var.db_username
   password               = var.db_password
-  vpc_security_group_ids = [aws_security_group.wk22Project-db-sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.wk22Project-dbsubnet-grp.name 
+  vpc_security_group_ids = [aws_security_group.wk22project-db-sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.wk22project-dbsubnet-grp.name 
   skip_final_snapshot    = true
 }
 
 #create a launch template for an EC2 instance using auto scaling group
 
-resource "aws_launch_template" "wk22Project-WebServer" {
-  name                   = "wk22Project-launch-template"
+resource "aws_launch_template" "wk22project-webserver" {
+  name                   = "wk22project-launch-template"
   image_id               = var.ami_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.wk22Project-webserver-sg.id]
+  vpc_security_group_ids = [aws_security_group.wk22project-webserver-sg.id]
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "Apache-WebServer"
+      Name = "apache-webserver"
     }
   }
   user_data = filebase64("apache.sh")
@@ -317,64 +315,64 @@ resource "aws_launch_template" "wk22Project-WebServer" {
 
 #auto scaling group to launch minimum of 2 instances and maximum of 5 instances
 
-resource "aws_autoscaling_group" "wk22Project-asg" {
+resource "aws_autoscaling_group" "wk22project-asg" {
   desired_capacity    = 2
   max_size            = 5
   min_size            = 2
-  vpc_zone_identifier = [aws_subnet.wk22Project-private1.id, aws_subnet.wk22Project-private2.id]
+  vpc_zone_identifier = [aws_subnet.wk22project-private1.id, aws_subnet.wk22project-private2.id]
 
   lifecycle {
     ignore_changes = [load_balancers, target_group_arns]
   }
 
   launch_template {
-    id = aws_launch_template.wk22Project-WebServer.id
+    id = aws_launch_template.wk22project-webserver.id
   }
 
   tag {
     key                 = "Name"
-    value               = "wk22Project-WebServer"
+    value               = "wk22project-webserver"
     propagate_at_launch = true
   }
 }
 
-resource "aws_autoscaling_attachment" "wk22Project-asg-attach" {
-  autoscaling_group_name = aws_autoscaling_group.wk22Project-asg.id
-  lb_target_group_arn    = aws_lb_target_group.wk22Project-launch-template.arn
+resource "aws_autoscaling_attachment" "wk22project-asg-attach" {
+  autoscaling_group_name = aws_autoscaling_group.wk22project-asg.id
+  lb_target_group_arn    = aws_lb_target_group.wk22project-launch-template.arn
 }
 
 #Create Application Load Balancer
-resource "aws_lb" "wk22Project-alb" {
-  name               = "wk22Project-alb"
+resource "aws_lb" "wk22project-alb" {
+  name               = "wk22project-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.wk22Project-public1.id, aws_subnet.wk22Project-public2.id]
+  subnets            = [aws_subnet.wk22project-public1.id, aws_subnet.wk22project-public2.id]
 
   security_groups = [
-    aws_security_group.wk22Project-alb-sg.id,
+    aws_security_group.wk22project-alb-sg.id,
   ]
 
   tags = {
-    Name = "wk22Project-alb-sg"
+    Name = "wk22project-alb-sg"
   }
 }
 
-resource "aws_lb_listener" "wk22Project-http-listener" {
-  load_balancer_arn = aws_lb.wk22Project-alb.arn
+resource "aws_lb_listener" "wk22project-http-listener" {
+  load_balancer_arn = aws_lb.wk22project-alb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
 
-    target_group_arn = aws_lb_target_group.wk22Project-launch-template.arn
+    target_group_arn = aws_lb_target_group.wk22project-launch-template.arn
 
     type             = "forward"
   }
 }
 
-resource "aws_lb_target_group" "wk22Project-launch-template" {
+resource "aws_lb_target_group" "wk22project-launch-template" {
 
-  name        = "wk22Project-launch-template"
+  name        = "wk22project-launch-template"
 
   port        = 80
 
@@ -382,7 +380,7 @@ resource "aws_lb_target_group" "wk22Project-launch-template" {
 
   target_type = "instance"
 
-  vpc_id      = aws_vpc.wk22Project-vpc.id
+  vpc_id      = aws_vpc.wk22project-vpc.id
 
   health_check {
     healthy_threshold   = 4
@@ -394,5 +392,5 @@ resource "aws_lb_target_group" "wk22Project-launch-template" {
     matcher             = "300"
   }
 
-  depends_on = [aws_lb.wk22Project-alb]
+  depends_on = [aws_lb.wk22project-alb]
 }
--- output.tf
+++ output.tf
@@ -4,9 +4,3 @@ output "db-instance-endpoint" {
   description = "DB instance endpoint"
   value       = aws_db_instance.wk22project-db-instance.endpoint
 }
-
-
-output "wk22Project-WebServer_public_ips" {
-  description = "The public IP addresses of the web server-tier instances"
-  value = aws_launch_template.wk22Project-WebServer.public_ip
-}
